af9de2f0484fa1251091febf853dcc2190b052ac
okuzawats <okuzawats@gmail.com>
2023-02-08 22:01:24 +0900

    implement NextToken

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "local.packages/token"
+
 // 字句
 type Lexer struct {
 	// 入力値
@@ -31,4 +33,39 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+// 次の文字からtoken.Tokenを生成して返す。
+func (l *Lexer) NextToken() token.Token {
+	var t token.Token
+
+	switch l.ch {
+	case '=':
+		t = newToken(token.ASSIGN, l.ch)
+	case ';':
+		t = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		t = newToken(token.LPAREN, l.ch)
+	case ')':
+		t = newToken(token.RPAREN, l.ch)
+	case ',':
+		t = newToken(token.COMMA, l.ch)
+	case '+':
+		t = newToken(token.PLUS, l.ch)
+	case '{':
+		t = newToken(token.LBRACE, l.ch)
+	case '}':
+		t = newToken(token.RBRACE, l.ch)
+	case 0:
+		t.Literal = ""
+		t.Type = token.EOF
+	}
+
+	l.readChar()
+	return t
+}
+
+// token.Tokenを生成して返す。
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
