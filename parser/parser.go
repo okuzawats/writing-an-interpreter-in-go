e163f3204918a73981b7ecb827de329d40892aa6
okuzawats <okuzawats@gmail.com>
2023-02-11 14:18:47 +0900

    create parser package

---
+++ parser/parser.go
@@ -0,0 +1,33 @@
+package parser
+
+import (
+	"local.packages/ast"
+	"local.packages/lexer"
+	"local.packages/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// トークンを2つ読み込む。curTokenとpeekTokenがセットされる。
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
