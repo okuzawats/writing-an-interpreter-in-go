2002299992eb8b97812a5d633a7cee020ceb2b61
okuzawats <okuzawats@gmail.com>
2023-02-14 08:42:21 +0900

    add expression statement

--- ast/ast.go
+++ ast/ast.go
@@ -66,3 +66,14 @@ func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
 }
+
+// 式文
+type ExpressionStatement struct {
+	Token      token.Token // 式の最初のトークン
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+	return es.Token.Literal
+}
