172e1ce49176858f82c8ca2bd3963a89024b9769
okuzawats <okuzawats@gmail.com>
2024-10-14 22:36:25 +0900

    evaluate in repl

--- repl/go.mod
+++ repl/go.mod
@@ -1,3 +1,7 @@
 module okuzawats.com/go/repl
 
 go 1.23.1
+
+require local.packages/evaluator v0.0.0
+
+replace local.packages/evaluator => ../evaluator
--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"local.packages/evaluator"
 	"local.packages/lexer"
 	"local.packages/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
