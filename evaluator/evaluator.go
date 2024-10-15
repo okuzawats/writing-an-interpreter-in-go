535955b04a07f8624ecba4c733e82c6bd425f1fd
okuzawats <okuzawats@gmail.com>
2024-10-15 21:28:00 +0900

    前置演算子`-`の評価を実装

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -43,6 +43,8 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 	switch operator {
 	case "!":
 		return evalBangOperatorExpression(right)
+	case "-":
+		return evalMinusPrefixOperatorExpression(right)
 	default:
 		return NULL
 	}
@@ -59,6 +61,14 @@ func evalBangOperatorExpression(right object.Object) object.Object {
 	default:
 		return FALSE
 	}
+}
+
+func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
+	if right.Type() != object.INTEGER_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+	return &object.Integer{Value: -value}
 }
 
 // boolをBooleanに変換する時、キャッシュされているインスタンスを返すようにしている。
