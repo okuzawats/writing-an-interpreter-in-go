f0ccc029f217d8dbce1b060ded7c30b8dcd727bf
okuzawats <okuzawats@gmail.com>
2024-10-18 05:03:40 +0900

    if-elseを実装 (#29)

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -29,6 +29,10 @@ func Eval(node ast.Node) object.Object {
 		left := Eval(node.Left)
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.BlockStatement:
+		return evalStatements(node.Statements)
+	case *ast.IfExpression:
+		return evalIfExpression(node)
 	}
 	return nil
 }
@@ -111,6 +115,34 @@ func evalIntegerInfixExpression(operator string, left, right object.Object) obje
 		return nativeBoolToBooleanObject(leftVal != rightVal)
 	default:
 		return NULL
+	}
+}
+
+func evalIfExpression(ie *ast.IfExpression) object.Object {
+	condition := Eval(ie.Condition)
+
+	// truthyの場合はConsequenceを評価した結果を返す。
+	// truthyでない場合は、AlternativeがnullでなければAlternativeを評価した結果を返し、
+	// Alternativeがnullであれば `NULL` を返す。
+	if isTruthy(condition) {
+		return Eval(ie.Consequence)
+	} else if ie.Alternative != nil {
+		return Eval(ie.Alternative)
+	} else {
+		return NULL
+	}
+}
+
+func isTruthy(obj object.Object) bool {
+	switch obj {
+	case NULL:
+		return false
+	case TRUE:
+		return true
+	case FALSE:
+		return false
+	default:
+		return true
 	}
 }
 
