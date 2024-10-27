dbe666f468680111e792f5093790e06dee0dbdf1
okuzawats <okuzawats@gmail.com>
2024-10-27 15:04:06 +0900

    Monkey言語でFizzBuzzを実装 (#45)
    * main.go実行時にファイルを受け取った場合はファイルを読み込んで実行するように修正
    
    * Monkey言語でFizzBuzzを実装
    
    * GitHub ActionsでFizzBuzzを実行

--- main.go
+++ main.go
@@ -2,18 +2,56 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/user"
+
+	"okuzawats.com/go/evaluator"
+	"okuzawats.com/go/lexer"
+	"okuzawats.com/go/object"
+	"okuzawats.com/go/parser"
 
 	"okuzawats.com/go/repl"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
+	// 引数を受け取る。1つ目の引数は捨てる。
+	args := os.Args
+
+	if len(args) == 1 {
+		// REPLの起動
+		user, err := user.Current()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+		fmt.Printf("Feel free to tyep in commands\n")
+		repl.Start(os.Stdin, os.Stdout)
+	} else if len(args) == 2 {
+		// プログラムファイルの実行
+		file, err := os.Open(args[1])
+		if err != nil {
+			fmt.Println("Error opening file:", err)
+			return
+		}
+		defer file.Close()
+
+		content, err := io.ReadAll(file)
+		if err != nil {
+			fmt.Println("Error reading file:", err)
+			return
+		}
+
+		out := os.Stdout
+
+		l := lexer.New(string(content))
+		p := parser.New(l)
+
+		evaluated := evaluator.Eval(p.ParseProgram(), object.NewEnvironment())
+
+		if evaluated != nil && evaluated.Type() != object.NULL_OBJ {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Feel free to tyep in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
 }
